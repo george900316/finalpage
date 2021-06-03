eb39e58c4cf9d4930d8eb0c7063cb91f1b960246
george900316 <79505558+george900316@users.noreply.github.com>
2021-06-03 21:06:37 +0800

    Add files via upload

---
+++ js/final.search.js
@@ -0,0 +1,15 @@
+console.log("hi");
+$(document).ready(function(){
+    $(".form button").click(function(){
+    	alert("你要找的書在熱門排行 快去看看吧!!");
+    }); 
+});
+
+
+console.log("hi");
+$(document).ready(function(){
+    $(".buttons button").click(function(){
+    	alert("你要找的書在熱門排行 快去看看吧!!");
+    }); 
+});
+
